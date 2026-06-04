9af31503b1826847585c8c66fb8c01a9ac7fed72
Syed Mesum Ali Shah <s.mesumali99@gmail.com>
2026-06-04 18:35:32 +0500

    add seo

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://www.mesumali.xyz',
+      url: 'https://www.mesumali.xyz/',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
